ff7c600e31ee1fe23333bfb0eca6dc1a627aa363
redFrik <f@fredrikolofsson.com>
2021-11-24 14:59:59 +0100

    add optional sleep in autostart
    reference https://github.com/redFrik/supercolliderStandaloneRPI2/issues/3

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export QT_QPA_PLATFORM=offscreen
 export JACK_NO_AUDIO_RESERVATION=1
+#sleep 10 #optional - try with a little delay if autostart is not working
 ./sclang -a -l sclang.yaml mycode.scd
